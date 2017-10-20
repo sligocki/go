591b23c2ed77fa8c372c9475b57d0d916610bd54
Shawn Ligocki <sligocki@gmail.com>
2017-10-20 08:16:23 -0500

    More testing

--- zero/board_test.cc
+++ zero/board_test.cc
@@ -63,8 +63,56 @@ TEST(BoardTest, ScoreComplex) {
                             "..XO.\n"
                             "XXOO.\n"
                             "OOO..\n"
-                            "X.O..\n", &board));
+                            "X....\n", &board));
+  // Note: Large territory on right & bottom doesn't score because White needs
+  // to capture first.
+  EXPECT_EQ(9 - 7, board.Score());
+
+  board.SetPos(Pos(2, 4), Color::kWhite);
+  EXPECT_EQ("..XO.\n"
+            "..XO.\n"
+            "XXOO.\n"
+            "OOO..\n"
+            "X.O..\n", BoardToString(board));
+  // Now Left territory does count (bottom left still isn't resolved).
   EXPECT_EQ(9 - 15, board.Score());
 }
 
+TEST(BoardTest, Capture) {
+  Board board(5, 5);
+  EXPECT_TRUE(StringToBoard("XO...\n"
+                            ".....\n"
+                            ".....\n"
+                            ".....\n"
+                            ".....\n", &board));
+  EXPECT_EQ(1 - 1, board.Score());
+
+  // Captures one stone in corner.
+  board.PlayStone(Pos(0, 1), Color::kWhite);
+  EXPECT_EQ(".O...\n"
+            "O....\n"
+            ".....\n"
+            ".....\n"
+            ".....\n", BoardToString(board));
+  EXPECT_EQ(-25, board.Score());
+
+
+  EXPECT_TRUE(StringToBoard(".....\n"
+                            ".....\n"
+                            ".OXO.\n"
+                            "..O..\n"
+                            ".....\n", &board));
+  EXPECT_EQ(1 - 3, board.Score());
+
+  // Captures one stone in center.
+  board.PlayStone(Pos(2, 1), Color::kWhite);
+  EXPECT_EQ(".....\n"
+            "..O..\n"
+            ".O.O.\n"
+            "..O..\n"
+            ".....\n", BoardToString(board));
+  EXPECT_EQ(-25, board.Score());
+}
+
 }  // namespace go_zero
+
