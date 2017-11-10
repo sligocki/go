4a0c76bf5cb423c63cefcbb86c2c5a9d5aae6966
Terry Ligocki <ligocki@Terrys-MacBook-Pro.local>
2017-11-10 10:41:51 -0800

    Added command line options for independently setting the number of rows and columns...

--- zero/mcts_main.cc
+++ zero/mcts_main.cc
@@ -11,6 +11,8 @@
 #include "zero/player.h"
 
 DEFINE_int64(board_size, 19, "Go board size.");
+DEFINE_int64(board_size_cols, -1, "Number of columns in Go board.");
+DEFINE_int64(board_size_rows, -1, "Number of rows in Go board.");
 DEFINE_int64(komi, 7, "Komi - 0.5.");
 DEFINE_int64(mcts_searches_per_move, 1600, "Number of MCTS searches per move.");
 DEFINE_double(mcts_decay, 0.9,
@@ -19,12 +21,24 @@ DEFINE_double(mcts_decay, 0.9,
 namespace go_zero {
 
 void Main() {
+  if (FLAGS_board_size_cols == -1) {
+    FLAGS_board_size_cols = FLAGS_board_size;
+  }
+
+  if (FLAGS_board_size_rows == -1) {
+    FLAGS_board_size_rows = FLAGS_board_size;
+  }
+
   // Play on every position twice.
-  int max_moves = FLAGS_board_size * FLAGS_board_size * 2;
+  int max_moves = FLAGS_board_size_cols * FLAGS_board_size_rows * 2;
+
   UniformPlayer player;
-  Game game(FLAGS_board_size, FLAGS_board_size, FLAGS_komi);
+
+  Game game(FLAGS_board_size_cols, FLAGS_board_size_rows, FLAGS_komi);
+
   MCTS mcts(FLAGS_mcts_searches_per_move, max_moves, FLAGS_mcts_decay,
             game, &player);
+
   mcts.SelfPlayGame();
 
   std::cout << "Result: Winner: " << ColorToString(mcts.state().winner())
@@ -35,5 +49,6 @@ void Main() {
 
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   go_zero::Main();
 }
